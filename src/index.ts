51da0c4ddd133c4e8d24b74189c91b83ee5c5dc0
Harish Raju <harisharaju1@gmail.com>
2021-05-07 19:18:02 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import "reflect-metadata";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { COOKIE_NAME, __prod__ } from "./constants";
+import { MyContext } from "./types";
+import cors from "cors";
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  const app = express();
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10years
+        httpOnly: true,
+        secure: __prod__,
+        sameSite: "lax", //protect agaisnt csrf
+      },
+      //saveUninitialized: false,
+      secret: "sfhsyjagfsd",
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log("server running on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
